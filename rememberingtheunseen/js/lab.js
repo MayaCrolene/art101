4264a272fad61002c2628786129c1c76bb50480f
MayaCrolene <mcrolene1@gmail.com>
2024-04-18 00:51:58 -0700

    Remembering the unseen
    Been a sec

---
+++ rememberingtheunseen/js/lab.js
@@ -0,0 +1,14 @@
+
+//lab.js - This simple JavaScript/jQuery script appends new elements to an output div
+   // index.js - Lab 16: JSON and APIs
+
+   //Requirements: jQuery must be loaded for this script to work.
+
+   //Author: Maya Crolene
+   //Date: December 6, 2023
+
+
+
+//Based of what we worked on in class
+console.log ("JS Loaded");
+
